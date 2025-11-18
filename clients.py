1da047465c5ae857bf8c413aa12bf9d0f70f6b12
hamidkrml <karimlihamid4@gmail.com>
2025-11-18 16:45:52 +0300

    Refactor client and server code to allow selection between TCP and UDP protocols

--- clients.py
+++ clients.py
@@ -1,12 +1,25 @@
 import socket
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("Hangi istemci başlatılsın? (1: TCP, 2: UDP)")
+secim = input("Seçiminizi girin: ")
 server_address = ('localhost', 12345)
 
-message = "Hello python from client"
-client_socket.sendto(message.encode(), server_address)
-
-data, _ = client_socket.recvfrom(1024)
-print("Received message:", data.decode())
-
-client_socket.close()
+if secim == "1":
+    # TCP İstemci
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(server_address)
+    message = "Hello python from TCP client"
+    client_socket.sendall(message.encode())
+    data = client_socket.recv(1024)
+    print("Received message:", data.decode())
+    client_socket.close()
+elif secim == "2":
+    # UDP İstemci
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    message = "Hello python from UDP client"
+    client_socket.sendto(message.encode(), server_address)
+    data, _ = client_socket.recvfrom(1024)
+    print("Received message:", data.decode())
+    client_socket.close()
+else:
+    print("Geçersiz seçim!")
--- server.py
+++ server.py
@@ -1,17 +1,37 @@
 import socket
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('localhost', 12345)
-server_socket.bind(server_address)
+print("Hangi sunucu başlatılsın? (1: TCP, 2: UDP)")
+secim = input("Seçiminizi girin: ")
 
-print("server calling started")
-
-while True:
-    print("Waiting for a message...")
-    data, client_address = server_socket.recvfrom(1024)
-    print("Received message:", data.decode())
-
-    message = "Hello from server"
-    server_socket.sendto(message.encode(), client_address)
-    # Sunucu sürekli çalışsın istiyorsanız break'i kaldırabilirsiniz
-    break
+if secim == "1":
+    # TCP Sunucu
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 12345)
+    server_socket.bind(server_address)
+    server_socket.listen(1)
+    print("TCP server started")
+    while True:
+        print("Waiting for a TCP connection...")
+        conn, client_address = server_socket.accept()
+        print("TCP connection from", client_address)
+        data = conn.recv(1024)
+        print("Received message:", data.decode())
+        message = "Hello from TCP server"
+        conn.sendall(message.encode())
+        conn.close()
+        break
+elif secim == "2":
+    # UDP Sunucu
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ('localhost', 12345)
+    server_socket.bind(server_address)
+    print("UDP server started")
+    while True:
+        print("Waiting for a UDP message...")
+        data, client_address = server_socket.recvfrom(1024)
+        print("Received message:", data.decode())
+        message = "Hello from UDP server"
+        server_socket.sendto(message.encode(), client_address)
+        break
+else:
+    print("Geçersiz seçim!")
